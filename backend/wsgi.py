c3b88d23b9730c80b1035541d383227a949b1fd4
Prajwal Kamte <kamteprajwal@gmail.com>
2025-10-30 09:02:16 +0530

    FINAL FIX: Implemented absolute path for stable model training on Render

---
+++ backend/wsgi.py
@@ -0,0 +1,7 @@
+import sys
+
+# Add your project directory to the path
+sys.path.insert(0, '/home/prajwal5065/Spam-Detector/')
+
+# Import the application object from app.py
+from app import app as application
